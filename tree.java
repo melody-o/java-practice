d133cfed8a439793b4927bfd352d6a4b02e27caa
Melody <98367521+melody-o@users.noreply.github.com>
2023-12-15 11:22:51 -0800

    making the tree

--- tree.java
+++ tree.java
@@ -2,5 +2,11 @@ public class Tree {
   public static void main(String[] args) {
   //I'm melody and we're going to print a tree to the screen.
   System.out.println("Hello! I'm Melody and learning to code in Java.");
+  System.out.println("I'm going to plant a tree today.");
+  System.out.println("      *  ");
+  System.out.println("     *** ");
+  System.out.println("    **** ");
+  System.out.println("   ******  ");
+  System.out.println("  ********  ");
 }
 }
