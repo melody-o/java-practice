43d4c3e59c4dfedc39ee1a565a02412283818c08
Melody <98367521+melody-o@users.noreply.github.com>
2023-12-15 11:14:57 -0800

    Create tree.java
    creating tree class and main

---
+++ tree.java
@@ -0,0 +1,6 @@
+public class Tree {
+  public static void main(String[] args) {
+  //I'm melody and we're going to print a tree to the screen.
+  System.out.println("Hello! I'm Melody and learning to code in Java.");
+}
+}
